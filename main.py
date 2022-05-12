a9eb730f0ee4f8b28532f197fef544aedfb8ead4
Alec German <kimarimt@protonmail.com>
2022-05-11 20:44:15 -0400

    Added game images and built rps game

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from game_images import game_images
+from random import randint
+
+
 def main():
-    pass
+    print('ROCK, PAPER, SCISSORS')
+    player = int(input('''
+Player's Turn
+================
+'0' for Rock
+'1' for Paper
+'2' for Scissors
+================
+'''))
+    print('\nYou chose:')
+    print(game_images[player])
+
+    computer = randint(0, 2)
+    print('\nComputer chose: ')
+    print(game_images[computer])
+
+    if player == computer:
+        print('IT\'S A TIE!')
+    elif player == 0 and computer == 2:
+        print('YOU WIN!')
+    elif player == 2 and computer == 0:
+        print('YOU LOSE')
+    elif player > computer:
+        print('YOU WIN!')
+    else:
+        print('YOU LOSE!')
 
 
-if __name__ == '__main___':
+if __name__ == '__main__':
     main()
